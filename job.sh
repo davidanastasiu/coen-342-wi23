c45c899a6e818bba54decc523c25df2bbf2bd894
David C. Anastasiu <danastasiu@scu.edu>
2023-01-24 09:29:13 -0800

    updated job.sh

--- job.sh
+++ job.sh
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16G
 #SBATCH --nodes=1
-#SBATCH --output=detection-%j.out
+#SBATCH --output=param-%j.out
 #SBATCH --time=10:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=USERNAME@scu.edu
 #
 module load PyTorch
-jupyter run /WAVE/users/danastasiu/coen-342-wi23/hyperparameter_tuning.ipynb > /WAVE/users/danastasiu/coen-342-wi23/hyperparameter_tuning.log
+python /WAVE/users/unix/danastasiu/coen-342-wi23/hyperparameter_tuning.py > /WAVE/users/unix/danastasiu/coen-342-wi23/hyperparameter_tuning.log
